0b1ae4ebf9820060fbc03779458d9845ff767992
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-08-07 17:39:01 -0400

    set_up_2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
- 
+console.log("Hello TypeScript!");
+
+import express from 'express';
+import http from 'http';
+import bodyParse from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+
+app.use(cookieParser());
+
+app.use(bodyParse.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
